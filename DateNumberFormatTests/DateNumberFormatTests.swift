7192fc3f846319e0ce44ee0d3b870fec1fdf3ef0
Jose Ines Cantu Arrambide <joseines@ponderosa.com.mx>
2016-08-17 13:34:36 -0500

    Agregue pruebas de multiples calendarios

--- DateNumberFormatTests/DateNumberFormatTests.swift
+++ DateNumberFormatTests/DateNumberFormatTests.swift
@@ -30,31 +30,70 @@ class DateNumberFormatTests: XCTestCase {
     // Convierte la fecha en numero, después la vuelva a crear en fecha y verifica que sean igual hasta el segundo.
     func testGregorianCalendar(){
         guard let calendar = NSCalendar(calendarIdentifier: NSCalendarIdentifierGregorian) else{
-            XCTFail()
             return
         }
         
+        XCTAssert(test(withCalendar: calendar))
+    }
+    
+    func testAllCalendars(){
+        let identifierArray = [NSCalendarIdentifierGregorian, NSCalendarIdentifierBuddhist,
+                               NSCalendarIdentifierChinese,
+                               NSCalendarIdentifierCoptic,
+                               NSCalendarIdentifierEthiopicAmeteMihret,
+                               NSCalendarIdentifierEthiopicAmeteAlem,
+                               NSCalendarIdentifierHebrew,
+                               NSCalendarIdentifierISO8601,
+                               NSCalendarIdentifierIndian,
+                               NSCalendarIdentifierIslamic,
+                               NSCalendarIdentifierIslamicCivil,
+                               NSCalendarIdentifierJapanese,
+                               NSCalendarIdentifierPersian,
+                               NSCalendarIdentifierRepublicOfChina
+        ]
+        
+        var failedCalendars = [String]()
+        for identifier in identifierArray{
+            guard let calendar = NSCalendar(calendarIdentifier: identifier) else{
+                failedCalendars.append(identifier)
+                
+            }
+            
+            if test(withCalendar: calendar) == false{
+                failedCalendars.append(identifier)
+            }
+        }
+        
+        if failedCalendars.count > 0 {
+            print("FAILED CALENDARS: \(failedCalendars)")
+            XCTFail()
+        }
+        else{
+            XCTAssert(true)
+        }
+        
+        
+    }
+    
+    func test(withCalendar calendar: NSCalendar) -> Bool{
         let originalDate = NSDate()
         
         // CONVERTIR A NUMERO
         guard let numberFormat = originalDate.psoNumberFormat(withCalendar: calendar) else{
-            XCTFail()
-            return
+            return false
         }
         
         
         // CONVERTIR A FECHA DE REGRESO
         guard let newDate = NSDate.psoDate(withNumberFormat: numberFormat) else{
-            XCTFail()
-            return
+            return false
         }
         
         print("\nOriginal Date: \(originalDate)\n New Date: \(newDate)")
         
         
         // VERIFICAR QUE SEA LA MISMA FECHA
-        XCTAssert(calendar.compareDate(originalDate, toDate: newDate, toUnitGranularity: [.Second]) == .OrderedSame)
-        
+        return calendar.compareDate(originalDate, toDate: newDate, toUnitGranularity: [.Second]) == .OrderedSame
     }
     
     func testPerformanceExample() {
