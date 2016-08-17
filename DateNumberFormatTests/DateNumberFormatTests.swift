e712cae633f291e9262b74166ed538fb3514e604
Jose Ines Cantu Arrambide <joseines@ponderosa.com.mx>
2016-08-17 13:20:41 -0500

    Agregue la prueba con el calendario gregoriano

--- DateNumberFormatTests/DateNumberFormatTests.swift
+++ DateNumberFormatTests/DateNumberFormatTests.swift
@@ -7,6 +7,7 @@
 //
 
 import XCTest
+
 @testable import DateNumberFormat
 
 class DateNumberFormatTests: XCTestCase {
@@ -24,6 +25,36 @@ class DateNumberFormatTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    // Convierte la fecha en numero, después la vuelva a crear en fecha y verifica que sean igual hasta el segundo.
+    func testGregorianCalendar(){
+        guard let calendar = NSCalendar(calendarIdentifier: NSCalendarIdentifierGregorian) else{
+            XCTFail()
+            return
+        }
+        
+        let originalDate = NSDate()
+        
+        // CONVERTIR A NUMERO
+        guard let numberFormat = originalDate.psoNumberFormat(withCalendar: calendar) else{
+            XCTFail()
+            return
+        }
+        
+        
+        // CONVERTIR A FECHA DE REGRESO
+        guard let newDate = NSDate.psoDate(withNumberFormat: numberFormat) else{
+            XCTFail()
+            return
+        }
+        
+        print("\nOriginal Date: \(originalDate)\n New Date: \(newDate)")
+        
+        
+        // VERIFICAR QUE SEA LA MISMA FECHA
+        XCTAssert(calendar.compareDate(originalDate, toDate: newDate, toUnitGranularity: [.Second]) == .OrderedSame)
+        
     }
     
     func testPerformanceExample() {
